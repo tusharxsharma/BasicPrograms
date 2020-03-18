bed2e43db24ed216374ec97ee8d00ac56e4d9709
Tushar Sharma <tusharsharmavj@gmail.com>
2020-03-19 00:07:46 +0530

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,first=0,second=1,next;
+	cout<<"Enter the number of times you want to print the fibonacci series \n";
+	cin>>num;
+	cout<<"Fibonacci Series \n";
+	for(int i=0;i<num;i++)
+	{
+	 cout<<first<<"\n";
+	 next=first+second;
+	 first=second;
+	 second=next;
+	}
+return -1;
+}
