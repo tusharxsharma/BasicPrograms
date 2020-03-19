b1688b7c4006cae52cb3fc9541a009cb51af1b32
Tushar Sharma <tusharsharmavj@gmail.com>
2020-03-19 08:47:24 +0530

    Update PrimeNumber.cpp

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 int main()
 {
-	int n,i,m=0;
-	cout<<"Enter the number to check the prime number : \n";
+	int n , x; 
+	cout <<"Enter the number : \n";
 	cin>>n;
-	m=n/2;
-	for(i=2;i<m;i++)
-		if(n%i==0)
-			cout<<"It is a prime number";
-		else
+	for(x=2;x<n;x++)
+	{
+		if(n%x==0)
 		{
-			cout<<"It is not a prime number";
-			cout<<i;
+			cout<<"Not Prime Number";
+			exit(0);
 		}
-		return 0;
+		else 
+		{
+			cout<<"Prime Number";
+			exit(1);
+		}
+	}
 }
