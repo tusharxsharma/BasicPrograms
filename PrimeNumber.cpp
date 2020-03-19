b68ef24c9faf796cc6ccad0e80e1180d981991d0
Tushar Sharma <tusharsharmavj@gmail.com>
2020-03-19 08:54:56 +0530

    Update PrimeNumber.cpp

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -5,17 +5,22 @@ int main()
 	int n , x; 
 	cout <<"Enter the number : \n";
 	cin>>n;
-	for(x=2;x<n;x++)
+	if(n<2)
+		cout<<"Number is lesser than 2 and not applicable";
+	else
 	{
-		if(n%x==0)
+		for(x=2;x<n/2;x++)
 		{
-			cout<<"Not Prime Number";
-			exit(0);
-		}
-		else 
-		{
-			cout<<"Prime Number";
-			exit(1);
+			if(n%x==0)
+			{
+				cout<<"Not Prime Number";
+				exit(0);
+			}
+			else 
+			{
+				cout<<"Prime Number";
+				exit(1);
+			}
 		}
 	}
 }
