a22a0cf75a3a73d2ab30f5254d349205e6413e3e
Tushar Sharma <tusharsharmavj@gmail.com>
2020-03-22 00:08:50 +0530

    Add files via upload

---
+++ Swapthirdvar.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter Number a : ";
+	cin>>a;
+	cout<<"Enter Number b : ";
+	cin>>b;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+	cout<<"Swapped Numbers are : \n";
+	cout<<"a : "<<a<<"\n";
+	cout<<"b : "<<b;
+}
