d9fc91194dd36f84ff07749575fde74fee209182
Tushar Sharma <tusharsharmavj@gmail.com>
2020-03-22 00:08:15 +0530

    Add files via upload

---
+++ ReverseNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,digit,rev=0;
+	cout<<"Enter the number to be reversed: ";
+	cin>>num;
+	while(num!=0)
+	{
+		digit=num%10;
+		rev=(rev*10)+digit;
+		num=num/10;
+	}
+	cout<<"The reversed number is :"<<rev;
+	
+	return -1 ;
+}
