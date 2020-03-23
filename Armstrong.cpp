68e3e15e965989abe2a05af5e9b5671bd5f3143b
Tushar Sharma <tusharsharmavj@gmail.com>
2020-03-23 21:29:49 +0530

    Update Armstrong.cpp

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
 int main()
-{
-	int n,r,sum=0,temp;
-	cout<<"Enter the number= ";
-	cin>>n;
-	temp=n;
-	while(n>0)
 	{
+		int n,r,sum=0,temp;
+		cout<<"Enter the number= ";
+		cin>>n;
+		temp=n;
+		while(n>0)
+		{
 		r=n%10;
 		sum=sum+(r*r*r);
 		n=n/10;
-	}
-	if(temp==sum)
-	{
+		}
+		if(temp==sum)
+		{
 		cout<<"Armstong Number";
-	}
+		}
 	else
 	{
 		cout<<"Not a Armstong Number";
